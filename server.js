5a1c0354d11547281c3b02d9e1a11aa47eb6f45b
furqanalimir <furkaanali89@gmail.com>
2021-08-28 18:11:00 +0530

    few changes

--- server.js
+++ server.js
@@ -23,10 +23,10 @@ app.use('/api/transactions', transaction);
 //server static if in production
 if(process.env.NODE_ENV === 'production'){
     // set static folder 
-    app.use(express.static('./client/build'));
+    app.use(express.static(path.resolve(__dirname, 'client','build')));
     
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname,  'client/build/index.html'));
+        res.sendFile(path.resolve(__dirname, 'client','build', 'index.html'));
     });
 }
 
